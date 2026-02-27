5d5886971707260a1eeaf22c5f058b164e4dc643
ryn-cx <262888154+ryn-cx@users.noreply.github.com>
2026-02-27 03:10:34 -0800

    Initial version

--- config/metadata.cjs
+++ config/metadata.cjs
@@ -1,26 +1,14 @@
-const {
-  author,
-  dependencies,
-  repository,
-  version,
-} = require("../package.json");
+const { author, repository, version } = require("../package.json");
 
 module.exports = {
-  name: {
-    $: "webpack-userscript-template",
-    cn: "中文名",
-    en: "english name",
-  },
-  namespace: "https://trim21.me/",
+  name: "Itamae",
+  namespace: "http://ryn.cx/",
   version: version,
   author: author,
   source: repository.url,
-  // 'license': 'MIT',
-  match: ["*://www.example.com/", "*://example.com/*"],
-  require: [
-    `https://cdn.jsdelivr.net/npm/jquery@${dependencies.jquery}/dist/jquery.min.js`,
-  ],
-  grant: ["GM.xmlHttpRequest"],
-  connect: ["httpbin.org"],
+  description: " Crunchyroll history exporter. ",
+  match: ["https://www.crunchyroll.com/history"],
+  require: [],
+  grant: "none",
   "run-at": "document-end",
 };
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,55 @@
 import "./style/main.less";
 
-//checkout homepage https://github.com/Trim21/gm-fetch for @trim21/gm-fetch
-import GM_fetch from "@trim21/gm-fetch";
+const WATCH_HISTORY_URL_REGEX = /\/content\/v2\/[^/]+\/watch-history/;
+const watchHistoryEntries: unknown[] = [];
 
-async function main() {
-  console.log("script start");
+const button = document.createElement("button");
 
-  // cross domain requests
-  console.log(`uuid: ${await fetchExample()}`);
+function updateButton() {
+  button.textContent = `Save List (${watchHistoryEntries.length})`;
 }
 
-async function fetchExample(): Promise<string> {
-  const res = await GM_fetch("https://httpbin.org/uuid");
-  const data = await res.json();
-  return data.uuid;
-}
+// Intercept XMLHttpRequest to capture watch-history API responses
+// Based on: https://stackoverflow.com/a/29293383
+(function (open: (...args: any[]) => void) {
+  XMLHttpRequest.prototype.open = function (
+    this: XMLHttpRequest & { _url?: string },
+    ...args: any[]
+  ) {
+    this._url = args[1]?.toString();
+    this.addEventListener(
+      "load",
+      function (this: XMLHttpRequest & { _url?: string }) {
+        if (this._url && WATCH_HISTORY_URL_REGEX.test(this._url)) {
+          const response = JSON.parse(this.responseText);
+          watchHistoryEntries.push(...response.data);
+          updateButton();
+        }
+      },
+    );
+    open.apply(this, args);
+  } as typeof XMLHttpRequest.prototype.open;
+})(XMLHttpRequest.prototype.open);
 
-main().catch((e) => {
-  console.log(e);
+button.id = "itamae-save-btn";
+button.textContent = "Save List (0)";
+
+button.addEventListener("click", () => {
+  const blob = new Blob([JSON.stringify(watchHistoryEntries, null, 2)], {
+    type: "application/json",
+  });
+
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = "watch-history.json";
+  a.click();
+  URL.revokeObjectURL(a.href);
 });
+
+const waitForH1 = setInterval(() => {
+  const h1 = document.querySelector("h1");
+  if (h1) {
+    clearInterval(waitForH1);
+    h1.appendChild(button);
+  }
+}, 200);
